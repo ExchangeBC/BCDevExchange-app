774281f50a056ad0d402e95031e38823f00f05e0
Anna Cox <annalisecox@gmail.com>
2015-02-24 10:10:44 -0800

    Remove use of winston-logentries. Always use console.

--- common/logging.js
+++ common/logging.js
@@ -1,5 +1,4 @@
 var winston = require('winston');
-var Logentries = require('winston-logentries');
 
 function Logging() {
     // always return the singleton instance, if it has been initialised once already
@@ -20,20 +19,13 @@ function Logging() {
         }
     };
 
-    if (process.env.NODE_ENV == 'production') {
 
-        this.logger = new winston.Logger({
-            transports: [new winston.transports.Logentries({token: 'f8c89aff-0de8-4ef5-b8b8-b977644c89b1'})],
-            levels: customLevels.levels
-        });
+    // use console logger
+    this.logger = new winston.Logger({
+        transports: [new (winston.transports.Console)()],
+        levels: customLevels.levels
+    });
 
-    } else {
-        // use console logger
-        this.logger = new winston.Logger({
-            transports: [new (winston.transports.Console)()],
-            levels: customLevels.levels
-        });
-    }
 
     // Setup some defaults
     this.logger.exitOnError = true;
