7d1cb61b9ad7e81fcb09c15baaeb7ab365af5b9d
Anna Cox <annalisecox@gmail.com>
2015-02-27 14:01:48 -0800

    display account details after login

--- public/account/account.js
+++ public/account/account.js
@@ -1,14 +1,22 @@
 'use strict';
 
-angular.module('bcdevxApp.account', ['ngRoute'])
+var app = angular.module('bcdevxApp.account', ['ngRoute', 'ngResource']);
 
-    .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/account', {
-            templateUrl: 'account/account.html',
-            controller: 'AccountCtrl'
-        });
-    }])
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/account', {
+        templateUrl: 'account/account.html',
+        controller: 'AccountCtrl'
+    });
+}]);
 
-    .controller('AccountCtrl', [function() {
+app.factory('Account', ['$resource', function($resource) {
+    return $resource('/account/:id');
+}]);
 
-    }]);
+app.controller('AccountCtrl', ['$scope', '$location', 'Account', function($scope, $location, Account) {
+
+    var account = Account.get({id: $location.search().id}, function(data) {
+        $scope.account = data;
+    });
+
+}]);
