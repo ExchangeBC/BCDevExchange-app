8e80e52e60bace19ef6c9a05b4a82327a0660462
Greg Turner <greg.fun@gmail.com>
2015-03-09 11:45:14 -0700

    added Angulartics

--- public/app.js
+++ public/app.js
@@ -2,13 +2,16 @@
 
 // Declare app level module which depends on views, and components
 var app = angular.module('bcdevxApp', [
-  'ngRoute',
-  'bcdevxApp.home',
-  'bcdevxApp.auth'
+    'ngRoute',
+    'bcdevxApp.home',
+    'bcdevxApp.auth',
+    'angulartics',
+    'angulartics.google.analytics'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/home'});
+    $routeProvider.otherwise({redirectTo: '/home'});
 }]);
+
 
 app.controller('IndexCtrl', function($scope, $location, $anchorScroll) {
 });
