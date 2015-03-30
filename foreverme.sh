b411d0eaef7436381f43a0c99833989975b66c94
Greg Turner <greg.fun@gmail.com>
2015-03-30 12:58:46 -0700

    auto sense directory

--- foreverme.sh
+++ foreverme.sh
@@ -10,7 +10,7 @@
 # See the License for the specific language governing permissions and limitations under the License.
 
 #!/bin/bash
-cwd=$(pwd)
+cwd=dirname $0
 APPLICATION_PATH=$cwd"/server.js"
 LOG=$cwd"/log/forever.log"
 STDOUT=$cwd"/log/stdout.log"
