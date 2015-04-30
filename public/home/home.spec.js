bc4e6ef17a4d7352cfa3087acccafb8cef67709b
Yiling Lu <yilingl@gmail.com>
2015-04-30 13:20:22 -0700

    testing stuff

---
+++ public/home/home.spec.js
@@ -0,0 +1,11 @@
+
+
+describe('Home Test Suites', function(){
+
+    describe("Test Suite 1", function(){
+        it("should pass", function(done){
+            console.log("I run and I pass.");
+            done();
+        })
+    });
+})
