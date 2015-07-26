7a9d48fa877154e37fe0a6f02774dc5c94112383
abbr <wenfred@gmail.com>
2015-07-26 13:40:57 -0700

    added yeoman angular-fullstack scaffolded project

---
+++ yeoman/client/app/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('yeomanApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'ui.bootstrap'
+])
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .otherwise({
+        redirectTo: '/'
+      });
+
+    $locationProvider.html5Mode(true);
+  });
---
+++ yeoman/client/components/navbar/navbar.controller.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('yeomanApp')
+  .controller('NavbarCtrl', function ($scope, $location) {
+    $scope.menu = [{
+      'title': 'Home',
+      'link': '/'
+    }];
+
+    $scope.isCollapsed = true;
+
+    $scope.isActive = function(route) {
+      return route === $location.path();
+    };
+  });
