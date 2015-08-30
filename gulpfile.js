7a622cb51fe14fe2c58400524e216815d871bd82
f-w <fred.wen@gov.bc.ca>
2015-08-29 23:14:46 -0700

    updated license

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ var concat = require('gulp-concat');
 
 var targetFile = ['!**/bower_components/**/', '!public/bootstrap/**/',
                     '!**/gulpfile.js', '!public/js/**/*.*',
-                    'public/**/*.html', 'public/**/*.js', 'app/**/*.js','config/**/*.json'];
+                    'public/**/*.html', 'public/**/*.js', 'app/**/*.js','config/**/*.json','*.js'];
 
 var licenseFile = 'copyright-apache2.txt';
 
