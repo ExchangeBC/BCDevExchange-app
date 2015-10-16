aaf43814b93fef8102d8fedbc99679781939b8ae
f-w <fred.wen@gov.bc.ca>
2015-10-16 13:48:37 -0700

    formatting

--- server/routes/resources.js
+++ server/routes/resources.js
@@ -1,16 +1,16 @@
 /*
-Copyright 2015 Province of British Columbia
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and limitations under the License.
-*/
+ Copyright 2015 Province of British Columbia
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and limitations under the License.
+ */
 
 var async = require('async')
 var request = require('request')
@@ -21,7 +21,9 @@ var crypto = require('crypto')
 var urlParser = require('url')
 var getResourcesFromArray = function (resourceList, success, error) {
   async.concat(resourceList, getCatalogueItems, function (err, results) {
-    if (err) error(err)
+    if (err){
+      error(err)
+    }
     else {
       success(results)
     }
@@ -217,31 +219,30 @@ function getGitHubCatalogueItems(catalogue, callback) {
 }
 
 function getCKANCatalogueItems(catalogue, callback) {
-    request({
-        url: catalogue.baseUrl + '/action/package_search?q=' + catalogue.tagName + ':' + catalogue.tagValue + '&rows=200',
-        timeout: 5000
-    }, function (error, response, body) {
-        if (!error &&
-                typeof response !== 'undefined' &&
-                response.statusCode === 200) {
+  request({
+    url: catalogue.baseUrl + '/action/package_search?q=' + catalogue.tagName + ':' + catalogue.tagValue + '&rows=200',
+    timeout: 5000
+  }, function (error, response, body) {
+    if (!error &&
+      typeof response !== 'undefined' &&
+      response.statusCode === 200) {
 
-            var json = JSON.parse(body)
+      var json = JSON.parse(body)
 
-            // remove extraneous info from result
-            async.concat(json.result.results, transformCKANResult, function (err, results) {
-                copyCatalogue(catalogue, results)
-                callback(err, results)
-            })
-        } else if (error) {
-            logger.error('Error while fetching %s content: %s, body: %s', catalogue.short_name, error, body)
-            callback({
-                'error': error.toString()
-            })
-        }
-        else{
-            callback(null,null)
-        }
-    })
+      // remove extraneous info from result
+      async.concat(json.result.results, transformCKANResult, function (err, results) {
+        copyCatalogue(catalogue, results)
+        callback(err, results)
+      })
+    } else if (error) {
+      logger.error('Error while fetching %s content: %s, body: %s', catalogue.short_name, error, body)
+      callback({
+        'error': error.toString()
+      })
+    } else {
+      callback(null, null)
+    }
+  })
 }
 
 function parseGitHubResourceResults(result, callback) {
@@ -282,22 +283,22 @@ function transformCKANResult(result, callback) {
 
   // trim the tags
   async.concat(result.tags, function (item, tagsCallback) {
-      tagsCallback(null, {
-        "display_name": item.display_name,
-        "colour": crypto.createHash('md5').update(item.display_name).digest("hex").substring(0, 6)
-      })
-    },
+    tagsCallback(null, {
+      "display_name": item.display_name,
+      "colour": crypto.createHash('md5').update(item.display_name).digest("hex").substring(0, 6)
+    })
+  },
     function (error, results) {
       transformed.tags = results
     })
 
   // trim the resources
   async.concat(result.resources, function (item, resourceCallback) {
-      resourceCallback(null, {
-        "name": item.name,
-        "url": item.url
-      })
-    },
+    resourceCallback(null, {
+      "name": item.name,
+      "url": item.url
+    })
+  },
     function (error, results) {
       transformed.resources = results
     })
