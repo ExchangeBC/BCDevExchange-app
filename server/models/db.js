c60988953378695ad68f8a487aa48d515ac67efb
f-w <fred.wen@gov.bc.ca>
2015-11-12 22:44:22 -0800

    use fa gh icon in profile; make account identities uniquie.

--- server/models/db.js
+++ server/models/db.js
@@ -25,22 +25,22 @@ var dbURI = config.mongodb.connectionString
 mongoose.connect(dbURI)
 
 mongoose.connection.on('connected', function () {
-  logger.info("Mongoose default connection open to " + dbURI)
+    logger.info("Mongoose default connection open to " + dbURI)
 })
 
 mongoose.connection.on('error', function (err) {
-  logger.error("Mongoose default connection error: " + err)
+    logger.error("Mongoose default connection error: " + err)
 })
 
 mongoose.connection.on('disconnected', function () {
-  logger.info("Mongoose default connection disconnected")
+    logger.info("Mongoose default connection disconnected")
 })
 
 process.on('SIGINT', function () {
-  mongoose.connection.close(function () {
-    logger.info("Mongoose default connection disconnected through app termination")
-    process.exit(0)
-  })
+    mongoose.connection.close(function () {
+        logger.info("Mongoose default connection disconnected through app termination")
+        process.exit(0)
+    })
 })
 
 
@@ -50,222 +50,229 @@ var models = require('./models')
 exports.Account = models.account
 exports.Profile = models.profile
 exports.getAccountsByOrigin = function (origin, callback) {
-  var query = models.account.find({
-    'identities.origin': origin
-  })
-  query.populate('profiles')
+    var query = models.account.find({
+        'identities.origin': origin
+    })
+    query.populate('profiles')
 
-  query.exec(function (err, output) {
-    callback(err, output)
-  })
-}
-
-exports.queryAccounts = function (query, callback) {
-  models.account.find(query)
-    .populate('profiles')
-    .exec(function (err, output) {
-      callback(err, output)
+    query.exec(function (err, output) {
+        callback(err, output)
     })
 }
 
+exports.queryAccounts = function (query, callback) {
+    models.account.find(query)
+            .populate('profiles')
+            .exec(function (err, output) {
+                callback(err, output)
+            })
+}
+
 exports.getAccountById = function (accountId, populateProfiles, callback) {
-  var query = models.account.findById(accountId)
+    var query = models.account.findById(accountId)
 
-  if (populateProfiles) {
-    query = query.populate('profiles')
-  }
+    if (populateProfiles) {
+        query = query.populate('profiles')
+    }
 
-  query.exec(function (err, output) {
-    callback(err, output)
-  })
+    query.exec(function (err, output) {
+        callback(err, output)
+    })
 }
 
 exports.getAccountByIdentity = function (identifier, populateProfiles, callback) {
-  var query = models.account.findOne({
-    'identities.identifier': identifier
-  })
+    var query = models.account.findOne({
+        'identities.identifier': identifier
+    })
 
-  if (populateProfiles) {
-    query = query.populate('profiles')
-  }
+    if (populateProfiles) {
+        query = query.populate('profiles')
+    }
 
-  query.exec(function (err, output) {
-    callback(err, output)
-  })
+    query.exec(function (err, output) {
+        callback(err, output)
+    })
 }
 
 exports.createAccount = function (extProfile, accessToken, refreshToken, callback) {
 
-  var profile = new models.profile({
-    type: 'Individual',
-    name: {},
-    visible: true,
-    contact: {
-      email: {}
-    },
-    contactPreferences: {
-      notifyMeOfAllUpdates: true
-    }
-  })
-
-  profile.save(function (err) {
-    if (err) {
-      logger.error(err)
-      callback(err, null)
-    }
-
-    var user = new models.account({
-      identities: [{
-          origin: extProfile.provider,
-          identifier: extProfile.id, // User's identifier from origin
-          accessToken: accessToken,
-          refreshToken: refreshToken,
-          attributes: []
-        }],
-      profiles: [profile]
+    var profile = new models.profile({
+        type: 'Individual',
+        name: {},
+        visible: true,
+        contact: {
+            email: {}
+        },
+        contactPreferences: {
+            notifyMeOfAllUpdates: true
+        }
     })
-    user.save(function (err) {
-      if (err) {
-        logger.error(err)
-        callback(err, null)
-      }
 
-      callback(null, user)
+    profile.save(function (err) {
+        if (err) {
+            logger.error(err)
+            callback(err, null)
+        }
+
+        var user = new models.account({
+            identities: [{
+                    origin: extProfile.provider,
+                    identifier: extProfile.id, // User's identifier from origin
+                    accessToken: accessToken,
+                    refreshToken: refreshToken,
+                    attributes: []
+                }],
+            profiles: [profile]
+        })
+        user.save(function (err) {
+            if (err) {
+                logger.error(err)
+                callback(err, null)
+            }
+
+            callback(null, user)
+        })
     })
-  })
 
 }
 
 exports.addIdentity = function (account, extProfile, accessToken, refreshToken, callback) {
-  account.identities.push({
-    origin: extProfile.provider,
-    identifier: extProfile.id,
-    accessToken: accessToken,
-    refreshToken: refreshToken
-  })
-  account.save(function (err) {
-    if (err) {
-      logger.error(err)
-      callback(err, null)
-    }
+    account.identities.push({
+        origin: extProfile.provider,
+        identifier: extProfile.id,
+        accessToken: accessToken,
+        refreshToken: refreshToken
+    })
+    _.uniq(account.identities, function (e) {
+        return JSON.stringify({
+            origin: e.origin,
+            identifier: e.identifier,
+            accessToken: e.accessToken,
+            refreshToken: e.refreshToken
+        })
+    })
+    account.save(function (err) {
+        if (err) {
+            logger.error(err)
+            callback(err, null)
+        }
 
-    callback(null, account)
-  })
-
+        callback(null, account)
+    })
 }
 
 exports.countGitHubAccounts = function (callback) {
-  models.account.count({
-    'identities.origin': 'github'
-  }, function (err, result) {
-    callback(err, result)
-  })
+    models.account.count({
+        'identities.origin': 'github'
+    }, function (err, result) {
+        callback(err, result)
+    })
 }
 
 exports.countLinkedInAccounts = function (callback) {
-  models.account.count({
-    'identities.origin': 'linkedin'
-  }, function (err, result) {
-    callback(err, result)
-  })
+    models.account.count({
+        'identities.origin': 'linkedin'
+    }, function (err, result) {
+        callback(err, result)
+    })
 }
 
 exports.countDualAccounts = function (callback) {
-  // get all accounts which have a github AND linkedin identity
-  models.account.where({
-    $and: [{
-        'identities.origin': 'github'
-      }, {
-        'identities.origin': 'linkedin'
-      }]
-  }).count(function (err, result) {
-    callback(err, result)
-  })
+    // get all accounts which have a github AND linkedin identity
+    models.account.where({
+        $and: [{
+                'identities.origin': 'github'
+            }, {
+                'identities.origin': 'linkedin'
+            }]
+    }).count(function (err, result) {
+        callback(err, result)
+    })
 }
 
 exports.getPrograms = function (cb) {
-  var deferred = Q.defer()
-  models.program.find().select('-_id -__v').lean().exec(function (err, res) {
-    if (err)
-      deferred.reject(err)
-    else
-      deferred.resolve(res)
-  })
-  return deferred.promise.nodeify(cb)
+    var deferred = Q.defer()
+    models.program.find().select('-_id -__v').lean().exec(function (err, res) {
+        if (err)
+            deferred.reject(err)
+        else
+            deferred.resolve(res)
+    })
+    return deferred.promise.nodeify(cb)
 }
 
 exports.getProgram = function (programId, cb) {
-  var deferred = Q.defer()
-  models.program.findOne({
-    'id': programId
-  }).select('-_id -__v').lean().exec(function (err, res) {
-    if (err)
-      deferred.reject(err)
-    else
-      deferred.resolve(res)
-  })
-  return deferred.promise.nodeify(cb)
+    var deferred = Q.defer()
+    models.program.findOne({
+        'id': programId
+    }).select('-_id -__v').lean().exec(function (err, res) {
+        if (err)
+            deferred.reject(err)
+        else
+            deferred.resolve(res)
+    })
+    return deferred.promise.nodeify(cb)
 }
 
 exports.getProgramByName = function (programNm, cb) {
-  var deferred = Q.defer()
-  models.program.findOne({
-    'name': {$regex: new RegExp(programNm, "i")}
-  }).select('-_id -__v').lean().exec(function (err, res) {
-    if (err)
-      deferred.reject(err)
-    else
-      deferred.resolve(res)
-  })
-  return deferred.promise.nodeify(cb)
+    var deferred = Q.defer()
+    models.program.findOne({
+        'name': {$regex: new RegExp(programNm, "i")}
+    }).select('-_id -__v').lean().exec(function (err, res) {
+        if (err)
+            deferred.reject(err)
+        else
+            deferred.resolve(res)
+    })
+    return deferred.promise.nodeify(cb)
 }
 
 exports.createProgram = function (program, cb) {
-  var deferred = Q.defer()
-  program.id = require('node-uuid').v4()
-  models.program.create(program, function (err, res) {
-    if (err)
-      deferred.reject(err)
-    else
-      deferred.resolve(res)
-  })
-  return deferred.promise.nodeify(cb)
+    var deferred = Q.defer()
+    program.id = require('node-uuid').v4()
+    models.program.create(program, function (err, res) {
+        if (err)
+            deferred.reject(err)
+        else
+            deferred.resolve(res)
+    })
+    return deferred.promise.nodeify(cb)
 }
 
 exports.updateProgram = function (programId, programPatch, cb) {
-  var deferred = Q.defer()
-  exports.getProgram(programId).then(function (res) {
-    var updatedProgram = _.merge(res, programPatch)
-    models.program.findOneAndUpdate({id: programId}, updatedProgram, function (err, res) {
-      if (err)
-        deferred.reject(err)
-      else
-        deferred.resolve(res)
+    var deferred = Q.defer()
+    exports.getProgram(programId).then(function (res) {
+        var updatedProgram = _.merge(res, programPatch)
+        models.program.findOneAndUpdate({id: programId}, updatedProgram, function (err, res) {
+            if (err)
+                deferred.reject(err)
+            else
+                deferred.resolve(res)
+        })
     })
-  })
-  return deferred.promise.nodeify(cb)
+    return deferred.promise.nodeify(cb)
 }
 
 exports.deleteProgram = function (programId, cb) {}
 
 exports.getNumber = function (query, cb) {
-  var deferred = Q.defer()
-  models.number.findOne(query).lean().exec(function (err, res) {
-    if (err)
-      deferred.reject(err)
-    else
-      deferred.resolve(res)
-  })
-  return deferred.promise.nodeify(cb)
+    var deferred = Q.defer()
+    models.number.findOne(query).lean().exec(function (err, res) {
+        if (err)
+            deferred.reject(err)
+        else
+            deferred.resolve(res)
+    })
+    return deferred.promise.nodeify(cb)
 }
 
 exports.updateNumber = function (id, doc, cb) {
-  var deferred = Q.defer()
-  models.number.where({_id: id}).update(doc, function (err, res) {
-    if (err)
-      deferred.reject(err)
-    else
-      deferred.resolve(res)
-  })
-  return deferred.promise.nodeify(cb)
+    var deferred = Q.defer()
+    models.number.where({_id: id}).update(doc, function (err, res) {
+        if (err)
+            deferred.reject(err)
+        else
+            deferred.resolve(res)
+    })
+    return deferred.promise.nodeify(cb)
 }
