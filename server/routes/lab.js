fafa3a6456be5eb7e6f93b51fc2501b65b054043
f-w <fred.wen@gov.bc.ca>
2016-01-06 17:17:04 -0800

    Successfully created parameterized jenkins project.

--- server/routes/lab.js
+++ server/routes/lab.js
@@ -1,12 +1,12 @@
 /*
  Copyright 2015 Province of British Columbia
- 
+
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
- 
+
  http://www.apache.org/licenses/LICENSE-2.0
- 
+
  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  See the License for the specific language governing permissions and limitations under the License.
@@ -167,11 +167,15 @@ module.exports = function (app, db, passport) {
           var availablePorts = _.difference(resHostPortRange, results)
           data.set('hostPort', availablePorts[Math.floor(availablePorts.length * Math.random())])
           data.save(function (err) {
-            // TODO: call Jenkins DSL seed job to creat a new job - following few lines of code doesn't work yet
+            // call Jenkins DSL seed job to creat a new job
             var postData = {
-              json: {parameter: [{name: 'ghProject', value: data.githubRepoOwner + '/' + data.githubRepo}]}
+              name: data.get('name'),
+              githubRepo: data.get('githubRepoOwner') + '/' + data.get('githubRepo'),
+              gitBranch: data.get('gitBranch'),
+              dockerfilePath: data.get('dockerfilePath'),
+              dockerPort: data.get('dockerPort')
             }
-            request.post(config.lab.jenkinsSeedJobBuildUrl + '?ghProject=sdf')
+            request.post(config.lab.jenkinsSeedJobBuildUrl)
               .auth(config.lab.jenkinsUser, config.lab.jenkinsApiToken)
               .form(postData).on('response', function (response) {
               callback(err, data)
