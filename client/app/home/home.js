df1dca31c24a97a8062e0c156efa0023d1fc0f39
Shea Phillips <shea.phillips@gmail.com>
2016-01-11 11:43:42 -0800

    - closes #224 - calling the "NumbersService" is suppressed, and showing the numbers bar are both suppressed, based on the value of the "ui.enableNumbersBar" filed in the config file(s). - the config/default.json value for ui.enableNumbersBar is "false", so it will not show unless explicitly enable in another file.

--- client/app/home/home.js
+++ client/app/home/home.js
@@ -1,38 +1,40 @@
 /*
-Copyright 2015 Province of British Columbia
+ Copyright 2015 Province of British Columbia
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and limitations under the License.
-*/
+ Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and limitations under the License.
+ */
 
 'use strict';
 angular.module('bcdevxApp.home', ['ngRoute'])
 
-.factory('NumbersCountService', ['$resource', function($resource) {
+  .factory('NumbersCountService', ['$resource', function ($resource) {
     return $resource('/api/numbers');
-}])
+  }])
 
-.controller('HomeCtrl', ['$scope', '$location', '$anchorScroll', 'NumbersCountService', function($scope, $location, $anchorScroll, NumbersCountService) {
-    $scope.numbers = {
+  .controller('HomeCtrl', ['$rootScope', '$scope', '$location', '$anchorScroll', 'NumbersCountService', function ($rootScope, $scope, $location, $anchorScroll, NumbersCountService) {
+
+    if ($rootScope.config.enableNumbersBar) {
+      $scope.numbers = {
         isLoaded: false,
         accounts: '-',
         resources: '-',
         projects: '-',
         analytics: {
-            users: '-'
+          users: '-'
         },
         tweet_count: '-',
         total_stars: '-'
-    };
+      };
 
-    NumbersCountService.get({}, function(data) {
+      NumbersCountService.get({}, function (data) {
         $scope.numbers.isLoaded = true;
         $scope.numbers.accounts = data.githubAccounts;
         $scope.numbers.resources = data.resources;
@@ -40,5 +42,7 @@ angular.module('bcdevxApp.home', ['ngRoute'])
         $scope.numbers.analytics = data.analytics || $scope.numbers.analytics;
         $scope.numbers.tweet_count = data.twitter_bcdev.count;
         $scope.numbers.total_stars = data.bcdevx.stargazers + data.bcgov.stargazers;
-    });
-}]);
+      });
+    }
+
+  }]);
