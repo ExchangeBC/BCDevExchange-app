7929984838f7f1b4009f14b7cf41383fdf78063c
Shea Phillips <shea.phillips@gmail.com>
2016-01-13 22:19:20 -0800

    - responsive and scrolling tweaks.

--- client/app/home/home.js
+++ client/app/home/home.js
@@ -14,22 +14,72 @@
 
 'use strict';
 angular.module('bcdevxApp.home', ['ngRoute'])
+  //thanks to http://jsfiddle.net/brettdewoody/y65G5/
+  .service('anchorSmoothScroll', function () {
 
-  .controller('HomeCtrl', ['$rootScope', '$scope', '$location', '$anchorScroll', 'NumbersCountService', function ($rootScope, $scope, $location, $anchorScroll, NumbersCountService) {
+    this.scrollTo = function (eID) {
 
-    $scope.scrollTo = function (id) {
+      // This scrolling function
+      // is from http://www.itnewb.com/tutorial/Creating-the-Smooth-Scroll-Effect-with-JavaScript
 
-      if ($location.hash() !== id) {
-        console.log("Scrolling to", id);
-        // set the $location.hash to `id` and
-        // $anchorScroll will automatically scroll to it
-        $location.hash(id);
-      } else {
-        // call $anchorScroll() explicitly,
-        // since $location.hash hasn't changed
-        $anchorScroll();
+      var startY = currentYPosition();
+      var stopY = elmYPosition(eID);
+      var distance = stopY > startY ? stopY - startY : startY - stopY;
+      if (distance < 100) {
+        scrollTo(0, stopY);
+        return;
       }
+      var speed = Math.round(distance / 100);
+      if (speed >= 20) speed = 20;
+      var step = Math.round(distance / 25);
+      var leapY = stopY > startY ? startY + step : startY - step;
+      var timer = 0;
+      if (stopY > startY) {
+        for (var i = startY; i < stopY; i += step) {
+          setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
+          leapY += step;
+          if (leapY > stopY) leapY = stopY;
+          timer++;
+        }
+        return;
+      }
+      for (var i = startY; i > stopY; i -= step) {
+        setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
+        leapY -= step;
+        if (leapY < stopY) leapY = stopY;
+        timer++;
+      }
+
+      function currentYPosition() {
+        // Firefox, Chrome, Opera, Safari
+        if (self.pageYOffset) return self.pageYOffset;
+        // Internet Explorer 6 - standards mode
+        if (document.documentElement && document.documentElement.scrollTop)
+          return document.documentElement.scrollTop;
+        // Internet Explorer 6, 7 and 8
+        if (document.body.scrollTop) return document.body.scrollTop;
+        return 0;
+      }
+
+      function elmYPosition(eID) {
+        var elm = document.getElementById(eID);
+        var y = elm.offsetTop;
+        var node = elm;
+        while (node.offsetParent && node.offsetParent != document.body) {
+          node = node.offsetParent;
+          y += node.offsetTop;
+        }
+        return y;
+      }
+
+    };
+
+  })
+  .controller('HomeCtrl', ['$rootScope', '$scope', '$location', 'anchorSmoothScroll', function ($rootScope, $scope, $location, anchorSmoothScroll) {
+    $scope.scrollTo = function (anchor) {
+      $location.hash(anchor);
+
+      // call $anchorScroll()
+      anchorSmoothScroll.scrollTo(anchor);
     }
-
-
   }]);
