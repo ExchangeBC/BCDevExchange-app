8f9963ab2d92aa8c6ee9adbab6a51f171287997b
f-w <fred.wen@gov.bc.ca>
2016-01-26 16:12:40 -0800

    added a spinner to developers page.

--- client/app/developers/developers.controller.js
+++ client/app/developers/developers.controller.js
@@ -1,13 +1,13 @@
-'use strict';
+'use strict'
 
 angular.module('bcdevxApp.developers', ['ngRoute', 'ngResource', 'bcdevxApp.services'])
-  .controller('DevelopersCtrl', function ($scope, $route, $resource) {
+  .controller('DevelopersCtrl', function ($scope, $route, $resource, usSpinnerService) {
 
-    var Issues = $resource("/api/issues/:program");
+    var Issues = $resource("/api/issues/:program")
 
-    Issues.get({  }, function(programIssues) {
+    Issues.get({}, function (programIssues) {
       //the response is wrapped in  a map, keyed with "issues" - we just want the value of issues (an array of issues)
-      $scope.issues = programIssues.issues;
-    });
-
-  });
+      $scope.issues = programIssues.issues
+      usSpinnerService.stop('spinner-issues-list')
+    })
+  })
